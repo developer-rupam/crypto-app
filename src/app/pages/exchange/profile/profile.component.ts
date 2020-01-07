78d1f9b7ce6e29010e7a2963bccd4b869b0ce669
rupamjana <rupam.jana@codomotive.com>
2020-01-08 02:42:16 +0530

    profile page validation

--- src/app/pages/exchange/profile/profile.component.ts
+++ src/app/pages/exchange/profile/profile.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Helper } from '../../../utils/helper';
+import { Router } from "@angular/router";
+import { Service } from "../../../utils/service";
+import { PROJECTNAMEALIAS } from "../../../utils/init"
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-profile',
@@ -7,7 +12,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  public firstName : string = '';
+  public lastName : string = '';
+  public emailAddress : string = '';
+  public phone : number ;
+  public dayOfDob : string = '';
+  public monthOfDob : string = '';
+  public yearOfDob : string = '';
+  public country : string = '';
+  public citizenship : string = '';
+  public fiatAmount : string = '';
+
+  constructor(public helper:Helper, public router:Router,public service:Service) { }
+
+  /*** function for saving profile data ***/
+  saveProfileData = () => {
+    if(
+      this.firstName !='' && this.lastName != '' &&
+      this.emailAddress != '' && this.phone != undefined 
+    ){
+      //TODO: call webservice here
+    }else{
+      this.helper.showAlert('Please Provide firstname,lastname,email and phone','error')
+    }
+  }
 
   ngOnInit() {
   }
